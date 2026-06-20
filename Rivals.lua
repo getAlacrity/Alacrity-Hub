e48aa82bf5f97b70fb859b71428b2e023af993f8
rty <s1130131@cljhs.tyc.edu.tw>
2026-06-21 07:45:59 +0800

    Refactor script loading and execution process
    Refactored script loading to store content in a variable before execution.

--- Rivals.lua
+++ Rivals.lua
@@ -1,2 +1,14 @@
-getgenv().SCRIPT_KEY = ""
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/4a5a3e4f556e22d225bc9dbedc2b5542d7c9bcb8303557675641158b7a2ce2df/download"))()
+local src = game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/4a5a3e4f556e22d225bc9dbedc2b5542d7c9bcb8303557675641158b7a2ce2df/download")
+
+print("Downloaded", #src)
+
+local func, err = loadstring(src)
+
+print("Compiled", func ~= nil, err)
+
+task.wait(5)
+
+print("Executing...")
+func()
+
+print("Finished")
