75bf18e4d0c386e5d9b55380562f16217dc2e914
rty <s1130131@cljhs.tyc.edu.tw>
2026-06-21 07:47:08 +0800

    Update Rivals.lua

--- Rivals.lua
+++ Rivals.lua
@@ -1,3 +1,4 @@
+getgenv().SCRIPT_KEY = ""
 local src = game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/4a5a3e4f556e22d225bc9dbedc2b5542d7c9bcb8303557675641158b7a2ce2df/download")
 
 print("Downloaded", #src)
